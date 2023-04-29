a5bdc6a9a09080d39d688961784db65ba275ae4d
IguanaAzul <ramon.griffo98@gmail.com>
2023-04-29 12:26:11 -0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// Trabalho de Projeto e Análise em Algoritmos - DCC UFMG.
+// Ramon Griffo Costa.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
